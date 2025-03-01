abe95e13f01f5d559856ee6edb3c1b72669eb3ff
Harrison Pak <harrisonpak8@gmail.com>
2025-03-01 01:31:30 -0500

    cant deploy

--- pages/api/test.js
+++
@@ -1,7 +0,0 @@
-// pages/api/index.js
-
-export default function handler(req, res) {
-  const { name, age, method } = req.query;
-
-  res.status(200).json({message: `${name} u ${age} u ${method}`});
-}
